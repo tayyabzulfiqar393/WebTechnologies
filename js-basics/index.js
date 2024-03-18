5ef0163bb70b043fe42abe268ec556f13d520b38
tayyabzulfiqar393 <tayyabzulfiqar348@gmail.com>
2024-03-19 01:05:45 +0500

    Forgot to commit previously
    Committed again because I forgot to commit previously.

---
+++ js-basics/index.js
@@ -0,0 +1,52 @@
+// let firstName = 'Tayyab';
+// let lastName = 'Zulfiqar';
+
+// console.log(firstName+' '+lastName);
+
+// let person = {
+//     name: 'Tayyab',
+//     age: 20,
+//     residence: 'F-71 Jubilee Town Lahore'
+// };
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.residence);
+
+// person['name'] = 'Rashid';
+
+// console.log(person['name']);
+
+// let ageSelection = 'age';
+
+// person[ageSelection] = 22;
+
+// console.log(person);
+
+//Arrays
+
+// let flatMembers = ['Tayyab', 'Rashid', 'Abdullah', 'Zain', 'Rizwan'];
+
+// console.log(flatMembers);
+// console.log(flatMembers[3]);
+
+// flatMembers[5] = 'Saqib';
+// console.log(flatMembers);
+
+// flatMembers.push('Hatim');
+// console.log(flatMembers);
+
+// console.log(flatMembers.length);
+
+// flatMembers.pop();
+// console.log(flatMembers);
+
+//Function
+
+
+function sum(number1, number2){
+    return number1+number2;
+}
+
+console.log(sum(3,4));
